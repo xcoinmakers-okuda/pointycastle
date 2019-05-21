7a766d0e17e724cceaa096222840f86051d1f8ff
Yuma Okuda <y.okuda@xcoinmakers.com>
2019-05-21 13:49:05 +0900

    SHA3Digestを修正

--- lib/digests/sha3.dart
+++ lib/digests/sha3.dart
@@ -110,11 +110,28 @@ class SHA3Digest extends BaseDigest implements Digest {
   }
 
   int doFinal(Uint8List out, int outOff) {
+    absorbBits(0x02, 2);
+
     _squeeze(out, outOff, _fixedOutputLength);
 
     reset();
 
     return digestSize;
+  }
+
+  void absorbBits(int data, int bits) {
+    if (bits < 1 || bits > 7) {
+      throw new StateError("'bits' must be in the range 1 to 7");
+    }
+    if ((_bitsInQueue % 8) != 0) {
+      throw new StateError("attempt to absorb with odd length queue");
+    }
+    if (_squeezing) {
+      throw new StateError("attempt to absorb while squeezing");
+    }
+    int mask = (1 << bits) - 1;
+    _dataQueue[_bitsInQueue >> 3] = data & mask;
+    _bitsInQueue += bits;
   }
 
   void _init(int bitLength) {
